aa957709165781e527877349d945b7fefa1b43d5
Dave Lewis <dave@dllewis.org>
2017-12-13 20:15:44 +0000

    Added another test

---
+++ src/NServiceBus.Gateway.Channels.HttpVNext.AcceptanceTests/When_doing_request_reply.cs
@@ -0,0 +1,81 @@
+﻿namespace NServiceBus.Gateway.Channels.HttpVNext.AcceptanceTests
+{
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using NUnit.Framework;
+
+    public class When_doing_request_reply : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Callback_should_be_fired()
+        {
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<SiteA>(
+                    b => b.When(async (bus, c) =>
+                    {
+                        var options = new SendOptions();
+                        options.RouteToSites("SiteB");
+                        c.Response = await bus.Request<int>(new MyRequest(), options);
+                        c.GotCallback = true;
+                    }))
+                .WithEndpoint<SiteB>()
+                .Done(c => c.GotCallback)
+                .Run();
+
+            Assert.IsTrue(context.GotCallback);
+            Assert.AreEqual(1, context.Response);
+        }
+
+        public class Context : ScenarioContext
+        {
+            public bool GotCallback { get; set; }
+            public int Response { get; set; }
+        }
+
+        public class SiteA : EndpointConfigurationBuilder
+        {
+            public SiteA()
+            {
+                EndpointSetup<DefaultServer>(c =>
+                {
+                    c.MakeInstanceUniquelyAddressable("1");
+                    c.EnableCallbacks();
+
+                    var gatewaySettings = c.Gateway();
+                    gatewaySettings.ChannelFactories(s => new HttpVNextChannelSender(), s => new HttpVNextChannelReceiver());
+
+                    gatewaySettings.AddReceiveChannel("http://localhost:25699/SiteA/", "httpVNext");
+                    gatewaySettings.AddSite("SiteB", "http://localhost:25699/SiteB/", "httpVNext");
+                });
+            }
+        }
+
+        public class SiteB : EndpointConfigurationBuilder
+        {
+            public SiteB()
+            {
+                EndpointSetup<DefaultServer>(c =>
+                {
+                    c.EnableCallbacks(makesRequests: false);
+                    var gatewaySettings = c.Gateway();
+                    gatewaySettings.ChannelFactories(s => new HttpVNextChannelSender(), s => new HttpVNextChannelReceiver());
+
+                    gatewaySettings.AddReceiveChannel("http://localhost:25699/SiteB/", "httpVNext");
+                });
+            }
+
+            public class MyRequestHandler : IHandleMessages<MyRequest>
+            {
+                public Task Handle(MyRequest request, IMessageHandlerContext context)
+                {
+                    return context.Reply(1);
+                }
+            }
+        }
+
+
+        public class MyRequest : ICommand
+        {
+        }
+    }
+}
