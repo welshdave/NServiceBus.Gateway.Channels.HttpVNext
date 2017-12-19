779f59e2ef68eb6983309791a30c915d2fc5ca20
Dave Lewis <dave@dllewis.org>
2017-12-19 11:14:41 +0000

    Added simple proxy test

---
+++ src/NServiceBus.Gateway.Channels.HttpVNext.AcceptanceTests/Proxy.cs
@@ -0,0 +1,114 @@
+﻿namespace NServiceBus.Gateway.Channels.HttpVNext.AcceptanceTests
+{
+    using System;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class Proxy
+    {
+        public void Start(string address, string addressToProxy)
+        {
+            siteToProxy = new Uri(addressToProxy);
+
+            cancellationTokenSource = new CancellationTokenSource();
+            cancellationToken = cancellationTokenSource.Token;
+
+            listener = new HttpListener();
+
+            listener.Prefixes.Add(address);
+
+            try
+            {
+                listener.Start();
+            }
+            catch (Exception ex)
+            {
+                var message = $"Failed to start listener for {address} make sure that you have admin privileges";
+                throw new Exception(message, ex);
+            }
+
+            Task.Run(ProcessRequests, CancellationToken.None);
+        }
+
+        public async Task Stop()
+        {
+            cancellationTokenSource?.Cancel();
+            listener?.Close();
+
+
+        }
+
+        async Task ProcessRequests()
+        {
+            while (!cancellationTokenSource.IsCancellationRequested)
+            {
+                var context = await listener.GetContextAsync().ConfigureAwait(false);
+
+                if (cancellationTokenSource.IsCancellationRequested)
+                {
+                    return;
+                }
+
+                await ProxyRequest(context, cancellationToken);
+            }
+        }
+
+        async Task ProxyRequest(HttpListenerContext context, CancellationToken token)
+        {
+            var client = new HttpClient();
+            try
+            {
+                var localPath = context.Request.Url.LocalPath;
+                var newRequest = new HttpRequestMessage
+                {
+                    RequestUri = new Uri($"{siteToProxy}{localPath}"),
+                    Method = new HttpMethod(context.Request.HttpMethod),
+                    Content = new StreamContent(context.Request.InputStream)
+                };
+
+                foreach (var header in context.Request.Headers.AllKeys)
+                {
+                    if (!header.Contains("."))
+                    {
+                        newRequest.Headers.TryAddWithoutValidation(header, context.Request.Headers[header]);
+                    }
+                }
+
+                using (var response = await client.SendAsync(newRequest, HttpCompletionOption.ResponseHeadersRead,
+                    cancellationToken))
+                {
+                    context.Response.StatusCode = (int)response.StatusCode;
+                    context.Response.ContentType = response.Content?.Headers.ContentType?.MediaType;
+
+                    foreach (var header in response.Headers)
+                    {
+                        context.Response.Headers[header.Key] = string.Join(",", header.Value);
+                    }
+
+                    context.Response.Headers.Remove("transfer-encoding");
+
+                    if (response.Content != null)
+                    {
+                        foreach (var contentHeader in response.Content.Headers)
+                        {
+                            context.Response.Headers[contentHeader.Key] = string.Join(",", contentHeader.Value);
+                        }
+                        context.Response.Close(await response.Content.ReadAsByteArrayAsync(), false);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            }
+        }
+
+        Uri siteToProxy;
+        SemaphoreSlim concurrencyLimiter;
+        HttpListener listener;
+        CancellationTokenSource cancellationTokenSource;
+        CancellationToken cancellationToken;
+    }
+}
---
+++ src/NServiceBus.Gateway.Channels.HttpVNext.AcceptanceTests/When_sending_a_message_to_a_site_behind_a_reverse_proxy.cs
@@ -0,0 +1,90 @@
+﻿namespace NServiceBus.Gateway.Channels.HttpVNext.AcceptanceTests
+{
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using NUnit.Framework;
+
+    public class When_sending_a_message_to_a_site_behind_a_reverse_proxy : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Should_be_able_to_reply_to_the_message()
+        {
+            var proxy = new Proxy();
+            proxy.Start("http://localhost:25999/ProxyA/", "http://localhost:25999/SiteA/");
+
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<Headquarters>(b => b.When(async (bus, c) => await bus.SendToSites(new[]
+                {
+                    "SiteA"
+                }, new MyRequest())))
+                .WithEndpoint<SiteA>()
+                .Done(c => c.GotResponseBack)
+                .Run();
+
+            await proxy.Stop();
+
+            Assert.IsTrue(context.GotResponseBack);
+        }
+
+        public class Context : ScenarioContext
+        {
+            public bool GotResponseBack { get; set; }
+        }
+
+        public class Headquarters : EndpointConfigurationBuilder
+        {
+            public Headquarters()
+            {
+                EndpointSetup<DefaultServer>(c =>
+                {
+                    var gatewaySettings = c.Gateway();
+                    gatewaySettings.ChannelFactories(s => new HttpVNextChannelSender(), s => new HttpVNextChannelReceiver());
+
+                    gatewaySettings.AddReceiveChannel("http://localhost:25999/Headquarters/", "httpVNext");
+                    gatewaySettings.AddSite("SiteA", "http://localhost:25999/ProxyA/", "httpVNext");
+                });
+            }
+
+            public class MyResponseHandler : IHandleMessages<MyResponse>
+            {
+                public Context Context { get; set; }
+
+                public Task Handle(MyResponse response, IMessageHandlerContext context)
+                {
+                    Context.GotResponseBack = true;
+                    return Task.FromResult(0);
+                }
+            }
+        }
+
+        public class SiteA : EndpointConfigurationBuilder
+        {
+            public SiteA()
+            {
+                EndpointSetup<DefaultServer>(c =>
+                {
+                    var gatewaySettings = c.Gateway();
+                    gatewaySettings.ChannelFactories(s => new HttpVNextChannelSender(), s => new HttpVNextChannelReceiver());
+
+                    gatewaySettings.AddReceiveChannel("http://localhost:25999/SiteA/", "httpVNext");
+                });
+            }
+
+            public class MyRequestHandler : IHandleMessages<MyRequest>
+            {
+                public Task Handle(MyRequest request, IMessageHandlerContext context)
+                {
+                    return context.Reply(new MyResponse());
+                }
+            }
+        }
+
+        public class MyRequest : IMessage
+        {
+        }
+
+        public class MyResponse : IMessage
+        {
+        }
+    }
+}
